808a82031e8a6bcfcf2b88f373c828abb80c8fcf
wkartist <2550115859@qq.com>
2020-06-17 23:50:10 +0800

    ,

--- node_mysql/index.js
+++ node_mysql/index.js
@@ -1,52 +1,26 @@
-
-
 require('./init')
-// const adminServ = require('./services/adminServices');
-// const moment = require('moment')
-const studentServ = require('./services/StudentServices');
-// require('./models/relation');
-studentServ.getStudents().then((r) => {
-    console.log(r)
+
+const express = require('express')
+// const http = require('http');
+const app = express();
+// const server = http.createServer(app);
+
+app.get('/abc', (req, res) => {
+    // req和res是被express封装后的对象
+    // res.send("<h1>abc</h1>")
+    // res.status(302).header("location","https://www.baidu.com").end()
+    // res.status(302).location("https://www.baidu.com").end()
+    res.redirect(302, "https://www.taobao.com")
+});
+
+app.listen(9527, () => {
+    console.log('server listen on 9527')
 })
-// studentServ.addStudent({
-//     name: 'wk',
-//     birthday: '2010-3-5',
-//     sex: true,
-//     mobile: '18734343434',
-//     ClassId: 12,
-// })
-// adminServ.addAdmin({
-//     loginId: 'dfsdfdsaf',
-//     loginPwd: '123456',
-//     name: 'dfsdsfd'
-// });
-
-// adminServ.updateAdmin(6,{
-//     loginPwd: '12345',
-// })
-// adminServ.login('df','12345').then((res) => {
-//     console.log(res)
-// })
-// console.log(moment.utc(1592379694955))
 
 
-// adminServ.deleteAdmin(2)
-// adminServ.updateAdmin(3, {
-//     loginId: 'wwwwwwwww'
-// })
-// require('./models/sync');
-// require('./models/relation');
-// require('./mock/mockStudent');
-// require('./mock/mockClass');
 
-// adminServ.login("abc", "abc").then((r) => {
-//     console.log(r);
-// })
 
-// adminServ.getAdminById(3).then((r) => {
-//     console.log(r)
-// })
-
-// studentServ.getStudents(1,10,true,'魏霞').then((r) => {
-//     console.log(r)
-// })
+// function listen(port, callback) {
+//     const http = require("http")
+//     http.createServer(this).listen(port, callback)
+// }
