de1e059f2337fb96e75f476f2761b9d43079edec
wkartist <2550115859@qq.com>
2020-06-19 00:04:12 +0800

    4.04常用中间件

---
+++ node_mysql/public/js/index.js
@@ -0,0 +1,3 @@
+document.getElementsByTagName('h1')[0].onclick = () => {
+    alert('hello')
+}
--- node_mysql/routes/init.js
+++ node_mysql/routes/init.js
@@ -1,42 +1,27 @@
 const express = require('express')
-// const http = require('http');
 const app = express();
-// const server = http.createServer(app);
+const path = require('path');
+const staticRoot = path.resolve(__dirname, "../public");
 
-app.use('*',require('./staticMiddleware'));
+//下面这段代码的作用：
+//请求时，会根据请求路径（req.path），从指定的目录中寻找是否存在该文件，
+//如果存在，直接响应文件，而不再移交给后面的中间件
+//如果不存在文件，则直接移交给后续的中间件处理
+app.use("/static",express.static(staticRoot))
 
-app.get(
-  '/abc', 
-  (req, res, next) => {
-    // req和res是被express封装后的对象
-    // res.send("<h1>abc</h1>")
-    // res.status(302).header("location","https://www.baidu.com").end()
-    // res.status(302).location("https://www.baidu.com").end()
-    // res.redirect(302, "https://www.taobao.com")
-    // res.status(200)
-    console.log('handler1')
-    // res.end()
-    // next()
-    throw new Error("abc")//相当于next(new Error("abc"))
-  },
-//   require("./errorMiddleware"),
-  (req, res, next) => {
-    console.log('33333')
-  }
-);
+// 解析 application/x-www-form-urlencoded 格式的请求体
+app.use(express.urlencoded({extended: true}))
 
-//错误处理的中间件需要4个参数，使用星号所有的中间件报错都会执行这个错误处理的中间件
+//解析 application/json格式的请求体
+app.use("*", express.json())
+
+app.post("/api/student", (req, res) => {
+  console.log(req.body);
+})
+
+// 处理错误的中间件
 app.use("*", require("./errorMiddleware"))
-//use可以匹配以第一个参数路径作为父路径的所有路径 /error /error/one
-// app.use("/error", require("./errorMiddleware"))
 
 app.listen(9528, () => {
     console.log('server listen on 9528')
 })
-
-
-
-// function listen(port, callback) {
-//     const http = require("http")
-//     http.createServer(this).listen(port, callback)
-// }
