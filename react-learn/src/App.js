946c542ac6fb73202211fe4e1b6a8333f3c2eb00
wkartist <2550115859@qq.com>
2020-07-05 00:35:26 +0800

    ,

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react'
-import Test from "./components/common/CheckBoxGroup/Test"
+import Test from './Test'
+import { A, B } from './components/Comps'
+import WithLog from './HOC/withLog'
+const LogA = WithLog(A)
+const LogB = WithLog(B)
+
 
 export default class App extends Component {
-    
+
     render() {
-        return <Test></Test>
+        console.log(LogA)
+        return <div>
+            <LogA a={1} />
+            <LogB />
+            <Test />
+        </div>
     }
 }
---
+++ react-learn/src/FuncDefault.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function FuncDefault(props) {
+    return (
+        <div>
+            a:{props.a}, b:{props.b}, c:{props.c}
+        </div>
+    )
+}
+
+FuncDefault.defaultProps = {
+    a: 1,
+    b: 2,
+    c: 3
+}
