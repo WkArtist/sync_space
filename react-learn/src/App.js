6254a77068cf33c500582e9b3ff74340d2ee3a94
wkartist <2550115859@qq.com>
2020-08-20 23:09:42 +0800

    ,

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,19 +1,13 @@
 import React, { useState, useEffect } from 'react'
-
+import { Provider } from "react-redux"
+import store from "./store"
+import Counter from './components/Counter'
 export default function App() {
-    const [n, setN] = useState(0)
-    // useEffect(() => {
-    //     setTimeout(() => {
-    //         console.log(n)
-    //     }, 5000)
-    // })
     return (
-        <div>
-            <h1>{n}</h1>
-            <button onClick={() => {
-                setN(n + 1)
-            }}>n+1</button>
-        </div>
+        <Provider store={store}>
+            <Counter />
+        </Provider>
+
     )
 }
 
