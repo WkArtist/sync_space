49e7cc8c217eb02dc51f7fdc077210832e65a49e
wkartist <2550115859@qq.com>
2020-09-01 22:52:02 +0800

    ，

--- typescript/moviesys/server/src/index.ts
+++ typescript/moviesys/server/src/index.ts
@@ -1,2 +1,17 @@
-let a:string = "dsfd";
-console.log('---')
+import "reflect-metadata"
+import { Movie } from "./entities/Movie"
+import { validate } from "class-validator";
+import { plainToClass } from "class-transformer";
+
+const m: any = {};
+m.name = 'wk';
+m.types = ["戏剧"];
+m.areas = ["中国大陆"];
+m.isClassic = true;
+m.timeLong = 2;
+
+const movie = plainToClass(Movie, m as object);
+console.log(movie)
+// validate(m).then(errors => {
+//     console.log(errors)
+// })
