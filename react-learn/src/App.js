b7f10e3dec083197ac4b28f66ff9b2af07abcbd8
wkartist <2550115859@qq.com>
2020-09-01 20:37:06 +0800

    ，

--- react-learn/src/App.js
+++ react-learn/src/App.js
@@ -1,12 +1,33 @@
-import React, { useState, useEffect } from 'react'
-import { Provider } from "react-redux"
-import store from "./store"
-import Counter from './components/Counter'
+import React, { Children } from 'react'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import qs from "query-string"
+function A(props) {
+    return <div>
+        <h1>组件A</h1>
+        <button onClick={() => {
+            props.history.push("/b");
+        }}>跳转到/b</button>
+    </div>
+}
+
+function B() {
+    return <h1>组件B</h1>
+}
+
+function NotFound() {
+    return <h1>找不到页面</h1>
+}
+
+
 export default function App() {
     return (
-        <Provider store={store}>
-            <Counter />
-        </Provider>
+        <Router>
+            <Switch>
+                <Route path="/a" component={A} ></Route>
+                <Route path="/b" component={B} ></Route>
+                <Route component={NotFound} />
+            </Switch>
+        </Router>
 
     )
 }
